2d05129be4b4a9815a368a0dc7b9311db685218d
Dario Campagna <campagna@esteco.com>
2025-10-06 15:44:35 +0200

    Initial commit

---
+++ src/main/java/HelloName.java
@@ -0,0 +1,7 @@
+void main(String... args) {
+    if (args.length == 0) {
+        System.out.println("Hello stranger!");
+    } else {
+        System.out.println("Hello " + args[0] + "!");
+    }
+}
